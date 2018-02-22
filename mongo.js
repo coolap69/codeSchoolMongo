5dd24edeba5bd83659873e333eed5fac35c84683
Louis Pazos <louispazos@gmail.com>
2018-02-22 01:39:04 +0000

    finished the exercises for level five

--- mongo.js
+++ mongo.js
@@ -75,3 +75,11 @@ db.monsters.find().count()
 db.monsters.sort({"Level":-1})
 
 }
+
+//Please complete the following syntax to group together our monsters by their type
+db.monsters.aggregate([ {"$group":{"_id":"$type"}}])
+
+//Please complete the following syntax to group the monsters together by their type, 
+//then include a count of all the monsters for that type and that types average attack. 
+//(the first three items are the answer to the prior question)
+db.monsters.aggregate([ {"$group":{"_id":"$type", "total":{"$sum":1}, "averageAttack":{"$avg":1}}}])
